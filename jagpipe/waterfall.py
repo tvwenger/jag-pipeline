e83517cab4e744093be1b6f70ec24611ff5a318d
Trey Wenger <tvwenger@gmail.com>
2021-12-28 13:18:55 -0800

    update waterfall to support scans

--- jagpipe/waterfall.py
+++ jagpipe/waterfall.py
@@ -33,23 +33,30 @@ from . import __version__
 
 
 def waterfall(
-    datafile, prefix, skiptime=1, skipchan=1, plotcal=False, plotflagged=False,
+    datafile,
+    prefix,
+    scans=None,
+    chanbin=None,
+    timebin=None,
+    plotcal=False,
+    plotflagged=False,
 ):
     """
-    Generate waterfall figure from a given SDHDF datafile, assuming data are
-    contiguous in time!
+    Generate waterfall figure from a given SDHDF datafile.
 
     Inputs:
         datafile :: string
             SDHDF filename
         prefix :: string
-            Filenames saved like "{prefix}_pseudoI.png"
-        skiptime :: integer
-            Downsample integrations by this amount (i.e., plot every
-            skiptime integration)
-        skipchan :: integer
-            Downsample channels by this amount (i.e., plot every
-            skipchan channel)
+            Filenames saved like "{prefix}_scan{num}_pseudoI.png"
+        scans :: list of integers
+            Scans to plot
+        chanbin :: integer
+            Number of channels to bin. If None, set so that there are
+            ~4,000 channel bins across the display range.
+        timebin :: integer
+            Number of integrations to bin. If None, set so that there
+            are ~8,000 integration bins across the display range.
         plotcal :: boolean
             If True, plot cal-on integrations
         plotflagged :: boolean
@@ -60,65 +67,109 @@ def waterfall(
     # Chunk cache size = 8 GB ~ 670 default chunks
     cache_size = 1024 ** 3 * 8
     with h5py.File(datafile, "r", rdcc_nbytes=cache_size) as sdhdf:
-        position = sdhdf["data"]["beam_0"]["band_SB0"]["scan_0"]["position"][::skiptime]
-        frequency = sdhdf["data"]["beam_0"]["band_SB0"]["frequency"][::skipchan]
-        freqtype = sdhdf["data"]["beam_0"]["band_SB0"]["frequency"].attrs["FRAME"]
-        flag = sdhdf["data"]["beam_0"]["band_SB0"]["scan_0"]["flag"]
-        cal = sdhdf["data"]["beam_0"]["band_SB0"]["scan_0"]["cal"][::skiptime]
-        data = sdhdf["data"]["beam_0"]["band_SB0"]["scan_0"]["data"]
+        # get scans
+        if scans is None:
+            scans = [
+                key
+                for key in sdhdf["data"]["beam_0"]["band_SB0"].keys()
+                if "scan" in key
+            ]
+        else:
+            scans = [
+                f"scan_{scan}"
+                for scan in scans
+                if f"scan_{scan}" in sdhdf["data"]["beam_0"]["band_SB0"].keys()
+            ]
 
-        extent = [
-            frequency[0] / 1.0e6,
-            frequency[-1] / 1.0e6,
-            (position["MJD"][-1] - position["MJD"][0]) * (24 * 3600),
-            0,
-        ]
+        # Get size of frequency and integration axes
+        frequency = sdhdf["data"]["beam_0"]["band_SB0"]["frequency"][()]
+        freqframe = sdhdf["data"]["beam_0"]["band_SB0"]["frequency"].attrs["FRAME"]
+
+        corr_idxs = [0, 1, 2, 3]
         datatypes = ["XX", "YY", "Re(XY)", "Im(XY)"]
         labels = ["XX", "YY", "ReXY", "ImXY"]
 
-        for datatype, label in zip(datatypes, labels):
-            # get data
-            print(f"Reading {datatype}")
-            if datatype == "XX":
-                plotdata = data[::skiptime, 0, ::skipchan]
-            elif datatype == "YY":
-                plotdata = data[::skiptime, 1, ::skipchan]
-            elif datatype == "Re(XY)":
-                plotdata = data[::skiptime, 2, ::skipchan]
-            elif datatype == "Im(XY)":
-                plotdata = data[::skiptime, 3, ::skipchan]
-            else:
-                raise ValueError(f"Invalid datatype: {datatype}")
-            plotflags = flag[::skiptime, ::skipchan]
+        for corr_idx, datatype, label in zip(corr_idxs, datatypes, labels):
+            for scani, scan in enumerate(scans):
+                # get data
+                data = sdhdf["data"]["beam_0"]["band_SB0"][scan]["data"]
+                flag = sdhdf["data"]["beam_0"]["band_SB0"][scan]["flag"]
+                metadata = sdhdf["data"]["beam_0"]["band_SB0"][scan]["metadata"]
+                num_int = data.shape[0]
 
-            if not plotcal:
-                # remove cal
-                plotdata[cal] = np.nan
+                # Set default values for bins if necessary
+                if chanbin is None:
+                    chanbin = max(1, int(len(frequency) / 4000))
+                    print(f"chanbin: {chanbin}")
+                if timebin is None:
+                    timebin = max(1, int(num_int / 4000))
+                    print(f"timebin: {timebin}")
 
-            if not plotflagged:
-                # apply flags
-                plotdata[plotflags] = np.nan
+                # Storage for plot data
+                plot_num_freq = int(np.ceil(len(frequency) / chanbin))
+                plot_num_int = int(np.ceil(data.shape[0] / timebin))
+                plotdata = np.empty((plot_num_int, plot_num_freq), dtype=float)
 
-            # Plot
-            print(f"Plotting {datatype}")
-            vmin, vmax = np.nanpercentile(plotdata, q=[5.0, 95.0])
-            fig, ax = plt.subplots(figsize=(12, 12))
-            cax = ax.imshow(
-                plotdata,
-                extent=extent,
-                interpolation="none",
-                aspect="auto",
-                vmin=vmin,
-                vmax=vmax,
-            )
-            fig.colorbar(cax, ax=ax, label="Relative Power")
-            ax.set_xlabel(f"{freqtype} Frequency (MHz)")
-            ax.set_ylabel(f"Seconds Since MJD = {position['MJD'][0]:.6f}")
-            ax.set_title(f"{datafile} {datatype}")
-            fig.tight_layout()
-            fname = f"{prefix}_{label}.png"
-            fig.savefig(fname, dpi=300, bbox_inches="tight")
-            plt.close(fig)
+                # bin in frequency and time
+                buffer_idx = 0
+                bin_idx = 0
+                data_buffer = np.ones((timebin, plot_num_freq), dtype=float) * np.nan
+                for i in range(data.shape[0]):
+                    if i % 10 == 0:
+                        print(
+                            f"Scan {scani}/{len(scans)}     "
+                            + f"Integration {i}/{num_int}   ",
+                            end="\r",
+                        )
+                    # bin in frequency
+                    dat = data[i, corr_idx, :]
+                    if not plotflagged:
+                        dat[flag[i]] = np.nan
+                    if not plotcal and metadata["CAL"][i]:
+                        dat[:] = np.nan
+                    pad = len(dat) % chanbin
+                    if pad != 0:
+                        pad = chanbin - pad
+                    dat = np.pad(dat, (0, pad), mode="constant", constant_values=np.nan)
+                    dat = np.nanmean(dat.reshape(-1, chanbin), axis=1)
+                    data_buffer[buffer_idx] = dat
+                    buffer_idx += 1
+
+                    # bin in time, save
+                    if (i == data.shape[0] - 1) or buffer_idx == timebin:
+                        plotdata[bin_idx] = np.nanmean(
+                            data_buffer[0:buffer_idx], axis=0
+                        )
+                        buffer_idx = 0
+                        bin_idx += 1
+
+                # plot extent
+                start_mjd = metadata["MJD"][0]
+                start_time = (metadata["MJD"][0] - start_mjd) * 24.0 * 3600.0
+                end_time = (metadata["MJD"][-1] - start_mjd) * 24.0 * 3600.0
+                start_freq = frequency[0] / 1e6
+                end_freq = frequency[-1] / 1e6
+                extent = [start_freq, end_freq, end_time, start_time]
+
+                # Generate figure
+                fig, ax = plt.subplots(figsize=(12, 12),)
+                vmin, vmax = np.nanpercentile(plotdata, q=[5.0, 95.0])
+                cax = ax.imshow(
+                    plotdata,
+                    extent=extent,
+                    interpolation="none",
+                    aspect="auto",
+                    vmin=vmin,
+                    vmax=vmax,
+                )
+                fig.colorbar(cax, ax=ax, label="Relative Power")
+                ax.set_xlabel(f"{freqframe} Frequency (MHz)")
+                ax.set_ylabel(f"Seconds Since MJD = {start_mjd:.6f}")
+                ax.set_title(f"{datafile} {scan} {datatype}")
+                fig.tight_layout()
+                fname = f"{prefix}_{scan}_{label}.png"
+                fig.savefig(fname, dpi=300, bbox_inches="tight")
+                plt.close(fig)
 
 
 def main():
@@ -137,10 +188,26 @@ def main():
         "prefix", type=str, help="Output plot files prefix",
     )
     parser.add_argument(
-        "-t", "--skiptime", type=int, default=1, help="Plot every n-th integration"
+        "-s",
+        "--scans",
+        type=int,
+        nargs="+",
+        default=None,
+        help="Scans to plot. Default: all scans",
     )
     parser.add_argument(
-        "-c", "--skipchan", type=int, default=1, help="Plot every n-th channel"
+        "-c",
+        "--chanbin",
+        type=int,
+        default=None,
+        help="Channel bin size. Default: 5000 bins across image",
+    )
+    parser.add_argument(
+        "-t",
+        "--timebin",
+        type=int,
+        default=None,
+        help="Time bin size. Default: 5000 bins across image",
     )
     parser.add_argument(
         "--plotcal",
@@ -155,8 +222,9 @@ def main():
     waterfall(
         args.datafile,
         args.prefix,
-        skiptime=args.skiptime,
-        skipchan=args.skipchan,
+        scans=args.scans,
+        chanbin=args.chanbin,
+        timebin=args.timebin,
         plotcal=args.plotcal,
         plotflagged=args.plotflagged,
     )
